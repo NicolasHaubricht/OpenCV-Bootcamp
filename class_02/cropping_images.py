f17e5b82bc72aa8062e0b86cd7802c430f90fa59
Nicolas <haubrichtnicolas@gmail.com>
2025-03-03 18:00:05 -0300

    Update Repository

--- class_02/cropping_images.py
+++ class_02/cropping_images.py
@@ -5,3 +5,7 @@ img_NZ_rgb = cv2.cvtColor(img_NZ_bgr, cv2.COLOR_BGR2RGB) # Convert the image to 
 
 plt.imshow(img_NZ_rgb) # Select the image to show
 plt.show() # Show the image
+
+cropped_region = img_NZ_rgb[200:400, 300:600] # Crop the image
+plt.imshow(cropped_region) # Select the image to show
+plt.show() # Show the image
--- main.py
+++ main.py
@@ -43,3 +43,15 @@ cb_img_copy[2:4, 2:4] = 200
 print(cb_img_copy)
 plt.imshow(cb_img_copy, cmap="gray")
 plt.show()
+
+# ================================ Cropping Images  ================================ #
+
+img_NZ_bgr = cv2.imread("class_02/assets/New_Zealand_Boat.jpg", 1) # Load the image with color mode
+img_NZ_rgb = cv2.cvtColor(img_NZ_bgr, cv2.COLOR_BGR2RGB) # Convert the image to RGB
+
+plt.imshow(img_NZ_rgb) # Select the image to show
+plt.show() # Show the image
+
+cropped_region = img_NZ_rgb[200:400, 300:600] # Crop the image
+plt.imshow(cropped_region) # Select the image to show
+plt.show() # Show the image
